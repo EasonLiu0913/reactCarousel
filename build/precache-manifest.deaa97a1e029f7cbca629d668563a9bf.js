61c1fd943e871993aa69710abce5d6be635761be
Eason Liu <onpica17@gmail.com>
2020-03-25 06:46:50 +0800

    add build folder

---
+++ build/precache-manifest.deaa97a1e029f7cbca629d668563a9bf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff04b54ddd5f280f080fd4565be37a2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2cab9d535a047c473b",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "ff03225842541bb69c95",
+    "url": "/static/css/main.b7ea93de.chunk.css"
+  },
+  {
+    "revision": "2c2cab9d535a047c473b",
+    "url": "/static/js/2.c8e60a2f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c8e60a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff03225842541bb69c95",
+    "url": "/static/js/main.27648d81.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  }
+]);
